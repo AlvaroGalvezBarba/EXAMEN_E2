bdd2ce57099396580dc6c0626025e97ee52b9ff1
AlvaroGalvezBarba <algaba01@floridauniversitaria.es>
2024-02-06 12:22:20 +0100

    pocimarevive

--- test/enanaTest.php
+++ test/enanaTest.php
@@ -23,20 +23,24 @@ class EnanaTest extends TestCase {
         #Se probará el efecto de una herida leve a una Enana con puntos de vida suficientes para sobrevivir al ataque
         #Se tendrá que probar que la vida es mayor que 0 y además que su situación es viva
         $enana = new Enana("Alvaro",100);
+        if ($enana->getPuntosVida() > 10) {
             $enana->heridaLeve();
             $enana->setSituacion("viva");
             $this->assertEquals(90, $enana->getPuntosVida());
             $this->assertEquals("viva", $enana->getSituacion());
+        }
     }
 
     public function testHeridaLeveMuere() {
         #Se probará el efecto de una herida leve a una Enana con puntos de vida insuficientes para sobrevivir al ataque
         #Se tendrá que probar que la vida es menor que 0 y además que su situación es muerta
         $enana = new Enana("Alvaro",5);
-        $enana->heridaLeve();
-        $enana->setSituacion("muerta");
-        $this->assertEquals(-5, $enana->getPuntosVida());
-        $this->assertEquals("muerta", $enana->getSituacion());
+        if ($enana->getPuntosVida() < 10) {
+            $enana->heridaLeve();
+            $enana->setSituacion("muerta");
+            $this->assertEquals(-5, $enana->getPuntosVida());
+            $this->assertEquals("muerta", $enana->getSituacion());
+        }
     }
 
     public function testHeridaGrave() {
@@ -52,7 +56,13 @@ class EnanaTest extends TestCase {
     public function testPocimaRevive() {
         #Se probará el efecto de administrar una pócima a una Enana muerta pero con una vida mayor que -10 y menor que 0
         #Se tendrá que probar que la vida es mayor que 0 y que su situación ha cambiado a viva
-
+        $enana = new Enana("Alvaro",-5);
+        if ($enana->getPuntosVida() > -10 && $enana->getPuntosVida() < 0){
+            $enana->pocima();
+            $enana->setSituacion("viva");
+            $this->assertEquals(15, $enana->getPuntosVida());
+            $this->assertEquals("limbo", $enana->getSituacion());
+        }
     }
 
     public function testPocimaNoRevive() {
