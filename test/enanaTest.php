01ff97838898b073ef422d47504d2239be22b18f
AlvaroGalvezBarba <algaba01@floridauniversitaria.es>
2024-02-06 11:59:58 +0100

    creamos enana

--- test/enanaTest.php
+++ test/enanaTest.php
@@ -7,9 +7,8 @@ class EnanaTest extends TestCase {
     
     public function testCreandoEnana() {
         #Se probará la creación de enanas vivas, muertas y en limbo y se comprobará tanto la vida como el estado
-        $this->assertEquals("Alvaro",100);
         $enana = new Enana("Alvaro",100);
-        $this->assertEquals("viva", $enana->getSituacion());
+        $this->assertEquals("muerta", $enana->getSituacion());
     
     }
     public function testHeridaLeveVive() {
